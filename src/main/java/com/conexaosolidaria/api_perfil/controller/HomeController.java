b6d2674793ef1cf537ac9e357cc88761eea2e629
luvieira <161535399+luvieirasantos@users.noreply.github.com>
2025-06-03 19:53:56 -0300

    Update HomeController.java

--- src/main/java/com/conexaosolidaria/api_perfil/controller/HomeController.java
+++ src/main/java/com/conexaosolidaria/api_perfil/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.conexaosolidaria.api_perfil.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HomeController {
     @GetMapping("/")
